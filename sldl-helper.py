146448faced5e967aba1855d1380e9d04c78a304
jwapico <altizer16@outlook.com>
2025-01-09 13:35:09 -0700

    update logging

--- sldl-helper.py
+++ sldl-helper.py
@@ -8,14 +8,14 @@ import shutil
 # https://github.com/fiso64/slsk-batchdl
 
 # downloads the song with title and artist from youtube using yt-dlp, writes logging information to a log file
-def download_song(title, artist, uri, output_path, log_file):
+def download_song(title, artist, uri, album, length, output_path, log_file):
     search_query = f"ytsearch:{title} {artist}"
     ytdlp_output = ""
 
     # append the logfile with a timestamp, track info, and yt-dlp output and print it
     with open(log_file, "a") as log:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        log.write(f"Title: {title}\nArtist: {artist}\nSpotify URI: {uri}\nTime: {timestamp}\nQuery: {search_query}\n\n")
+        log.write(f"Downloading from yt-dlp...\nTitle: {title}\nArtist: {artist}\nSpotify URI: {uri}\nTime: {timestamp}\nQuery: {search_query}\n\n")
 
         process = subprocess.Popen([
             "yt-dlp",
@@ -24,7 +24,7 @@ def download_song(title, artist, uri, output_path, log_file):
             "-x", "--audio-format", "mp3",
             "--embed-thumbnail", "--add-metadata",
             "--paths", output_path,
-            "-o", f"%(title)s.%(ext)s"
+            "-o", f"%(title)s - %(artist)s.%(ext)s"
         ], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
 
         for line in iter(process.stdout.readline, ''):
@@ -32,13 +32,23 @@ def download_song(title, artist, uri, output_path, log_file):
             log.write(line)
             ytdlp_output += line
 
-        seperator = "=" * 150
-        log.write(f"\n{seperator}\n\n")
-
         process.stdout.close()
         process.wait()
 
-        return ytdlp_output
+        if ytdlp_output.count(f"[download] Finished downloading playlist: {title} {artist}") > 0:
+            file_path_pattern = r'\[EmbedThumbnail\] ffmpeg: Adding thumbnail to "([^"]+)"'
+            match = re.search(file_path_pattern, ytdlp_output)
+            download_path = match.group(1) if match else ""
+        else:
+            download_path = ""
+
+        sldl_index_entry = f'"{download_path}",{artist},{album},{title},{length},0,1,0'
+        seperator = "=" * 150
+        log.write(f"\nFilepath: '{download_path}'")
+        log.write(f"\nSLDL Index Entry: {sldl_index_entry}")
+        log.write(f"\n\n{seperator}\n\n")
+
+        return download_path
 
 def main():
     # check that the number of arguments is correct
@@ -57,6 +67,7 @@ def main():
     state = sys.argv[8]
 
     OUTPUT_PATH = os.getcwd()
+    # TODO: put these in sldl-helper folder so they arent cluttering the music directory. also make them accessible to _index_fixer.py
     LOG_FILE_PATH = f"{OUTPUT_PATH}\\sldl-helper.log"
     NEW_INDEX_FILEPATH = F"{OUTPUT_PATH}\\sldl-helper_index.sldl"
 
@@ -70,16 +81,20 @@ def main():
     # sldl expects index entries to look like this: filepath,artist,album,title,length,tracktype,state,failurereason
     if state == "Downloaded":
         sldl_index_entry = f'"{path}",{artist},{album},{title},{length},{0},{1},{0}'
-        # TODO: append some info to the log file
+        with open(LOG_FILE_PATH, "a") as log_file:
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            log_file.write("Downloaded from SoulSeek...")
+            log_file.write(f"\nTitle: {title}\nArtist: {artist}\nSpotify URI: {uri}\nTime: {timestamp}")
+            log_file.write(f"\nFilepath: '{path}'")
+            log_file.write(f"\nSLDL Index Entry: {sldl_index_entry}")
+            seperator = "=" * 150
+            log_file.write(f"\n\n{seperator}\n\n")
     # if sldl did not find the song, download it from youtube
     elif state == "Failed":
-        ytdlp_output = download_song(title, artist, uri, OUTPUT_PATH, LOG_FILE_PATH)
+        download_path = download_song(title, artist, uri, album, length, OUTPUT_PATH, LOG_FILE_PATH)
         
         # if the download was successfull extract the filepath from the yt-dlp output and build the correct index entry
-        if ytdlp_output.count(f"[download] Finished downloading playlist: {title} {artist}") == 1:
-            file_path_pattern = r'\[EmbedThumbnail\] ffmpeg: Adding thumbnail to "([^"]+)"'
-            match = re.search(file_path_pattern, ytdlp_output)
-            download_path = match.group(1) if match else ""
+        if download_path != "":
             sldl_index_entry = f'"{download_path}",{artist},{album},{title},{length},0,1,0'
             print("DOWNLOAD SUCCESS")
         # if the download wasn't successfull from yt-dlp either, the index entry should reflect this
@@ -103,6 +118,7 @@ if __name__ == "__main__":
         main()
     except Exception as e:
         print(e)
+        # TODO: logging here too
         input("\n\nError occured, press enter to close")
     finally:
         pass
