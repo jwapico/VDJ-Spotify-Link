0995d8b0db6d0f3d059705b5570953dd803eca0f
jwapico <altizer16@outlook.com>
2025-01-09 22:07:45 -0700

    big changes

--- sldl-helper.py
+++ sldl-helper.py
@@ -17,6 +17,7 @@ def download_song(title, artist, uri, album, length, output_path, log_file):
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         log.write(f"Downloading from yt-dlp...\nTitle: {title}\nArtist: {artist}\nSpotify URI: {uri}\nTime: {timestamp}\nQuery: {search_query}\n\n")
 
+        # download the file using yt-dlp and necessary flags
         process = subprocess.Popen([
             "yt-dlp",
             search_query,
@@ -24,9 +25,10 @@ def download_song(title, artist, uri, album, length, output_path, log_file):
             "-x", "--audio-format", "mp3",
             "--embed-thumbnail", "--add-metadata",
             "--paths", output_path,
-            "-o", f"%(title)s - %(artist)s.%(ext)s"
+            "-o", f"%(title)s.%(ext)s"
         ], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
 
+        # print and append the output of yt-dlp to the log file
         for line in iter(process.stdout.readline, ''):
             print(line, end='')
             log.write(line)
@@ -42,7 +44,16 @@ def download_song(title, artist, uri, album, length, output_path, log_file):
         else:
             download_path = ""
 
-        sldl_index_entry = f'"{download_path}",{artist},{album},{title},{length},0,1,0'
+        title_artist_pattern = r'\[download\] Finished downloading playlist: (.+)'
+        match = re.search(title_artist_pattern, ytdlp_output)
+        extracted_text = match.group(1).lower()
+
+        if not title.lower() in extracted_text.lower():
+            log.write(f"\n\nTitle ({title}) not found in output: {extracted_text}\n\n")
+        if not artist.lower() in extracted_text.lower():
+            log.write(f"\n\nArtist ({artist}) not found in output: {extracted_text}\n\n")            
+
+        sldl_index_entry = f'"{download_path}","{artist}","{album}","{title}",{length},0,1,0'
         seperator = "=" * 150
         log.write(f"\nFilepath: '{download_path}'")
         log.write(f"\nSLDL Index Entry: {sldl_index_entry}")
@@ -67,9 +78,10 @@ def main():
     state = sys.argv[8]
 
     OUTPUT_PATH = os.getcwd()
-    # TODO: put these in sldl-helper folder so they arent cluttering the music directory. also make them accessible to _index_fixer.py
-    LOG_FILE_PATH = f"{OUTPUT_PATH}\\sldl-helper.log"
-    NEW_INDEX_FILEPATH = F"{OUTPUT_PATH}\\sldl-helper_index.sldl"
+    SLDL_HELPER_PATH = OUTPUT_PATH + "\\sldl-helper"
+    os.makedirs(SLDL_HELPER_PATH, exist_ok=True)
+    LOG_FILE_PATH = f"{SLDL_HELPER_PATH}\\sldl-helper.log"
+    NEW_INDEX_FILEPATH = F"{SLDL_HELPER_PATH}\\sldl-helper_index.sldl"
 
     # if our custom _index.sldl file doesn't already exist, create it with the header
     if not os.path.exists(NEW_INDEX_FILEPATH):
@@ -80,7 +92,7 @@ def main():
     # if sldl was able to find and download the song on SoulSeek, create the appropriate index entry
     # sldl expects index entries to look like this: filepath,artist,album,title,length,tracktype,state,failurereason
     if state == "Downloaded":
-        sldl_index_entry = f'"{path}",{artist},{album},{title},{length},{0},{1},{0}'
+        sldl_index_entry = f'"{path}","{artist}","{album}","{title}",{length},{0},{1},{0}'
         with open(LOG_FILE_PATH, "a") as log_file:
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             log_file.write("Downloaded from SoulSeek...")
@@ -95,11 +107,11 @@ def main():
         
         # if the download was successfull extract the filepath from the yt-dlp output and build the correct index entry
         if download_path != "":
-            sldl_index_entry = f'"{download_path}",{artist},{album},{title},{length},0,1,0'
+            sldl_index_entry = f'"{download_path}","{artist}","{album}","{title}",{length},0,1,0'
             print("DOWNLOAD SUCCESS")
         # if the download wasn't successfull from yt-dlp either, the index entry should reflect this
         else:
-            sldl_index_entry = f'"",{artist},{album},{title},{length},0,2,3'
+            sldl_index_entry = f'"","{artist}","{album}","{title}",{length},0,2,3'
             print("DOWNLOAD FAIL")
     else:
         raise Exception(f"Unexpected state: {state}")
@@ -117,8 +129,12 @@ if __name__ == "__main__":
     try:
         main()
     except Exception as e:
-        print(e)
-        # TODO: logging here too
-        input("\n\nError occured, press enter to close")
+        LOG_FILE_PATH = os.getcwd() + "\\sldl-helper\\sldl-helper.log"
+        with open(LOG_FILE_PATH, "a") as log_file:
+            seperator = "=" * 150
+            log_file.write(f"\n\n{seperator}\n\n")
+            log_file.write("ERROR CAUGHT:\n")
+            log_file.write(e)
+            log_file.write(f"\n\n{seperator}\n\n")
     finally:
         pass
