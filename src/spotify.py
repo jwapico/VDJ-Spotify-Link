c20d1d920b91f2e346f859682588d7341009dc04
jwapico <altizer16@outlook.com>
2025-01-15 19:03:04 -0700

    begin spotify integration and restructure project directory

---
+++ src/spotify.py
@@ -0,0 +1,64 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("client_id")
+CLIENT_SECRET = os.getenv("client_secret")
+REDIRECT_URI = os.getenv("redirect_uri")
+SPOTIFY_SCOPE = "user-library-read"
+spotipy_client = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SPOTIFY_SCOPE, client_id=CLIENT_ID,
+                                                           client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI))
+USER_ID = spotipy_client.current_user()["id"]
+
+def main():
+    playlists = get_all_playlists()
+    for playlist in playlists:
+        if playlist["name"] == "sldl":
+            # TODO: this only gets the first 100 tracks
+            get_all_playlist_tracks(playlist["id"])
+            # tracks = spotipy_client.playlist_tracks(playlist_id=playlist["id"])
+            # print(len(tracks["items"]))
+            # dump_json(tracks, "tracks.json")
+
+def dump_json(contents, filename):
+    with open("assets/" + filename, "w", encoding="utf-8") as file:
+        json.dump(contents, file)
+
+def get_all_playlists(json_filename=None):
+    playlists_info = spotipy_client.user_playlists(USER_ID, limit=1)
+    num_playlists = playlists_info["total"]
+
+    all_playlists = []
+    offset = 0
+
+    while offset < num_playlists:
+        new_playlists = spotipy_client.user_playlists(USER_ID, limit=50, offset=offset)
+        all_playlists.extend(new_playlists["items"])
+        offset += 50
+
+    if json_filename is not None:
+        dump_json(all_playlists, json_filename)
+
+    return all_playlists
+
+def get_all_playlist_tracks(playlist_id, json_filename=None):
+    all_tracks = []
+    offset = 0
+
+    while True:
+        response = spotipy_client.playlist_items(playlist_id=playlist_id, offset=offset)
+        all_tracks.extend(response["items"])
+        offset += 100
+
+        if len(response["items"]) < 100:
+            break
+        
+    if json_filename is not None:
+        dump_json(all_tracks, json_filename)
+
+if __name__ == "__main__":
+    main()
