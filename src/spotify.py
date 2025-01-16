0e8867b4e6dd4374de90657f3a4f76c4841f14a7
jwapico <altizer16@outlook.com>
2025-01-16 00:59:12 -0700

    begin vdjfolder creation

--- src/spotify.py
+++ src/spotify.py
@@ -3,6 +3,9 @@ from spotipy.oauth2 import SpotifyOAuth
 import json
 import os
 from dotenv import load_dotenv
+import xml.etree.ElementTree as ET
+from pathlib import Path
+from xml.dom import minidom
 
 load_dotenv()
 
@@ -10,25 +13,17 @@ CLIENT_ID = os.getenv("client_id")
 CLIENT_SECRET = os.getenv("client_secret")
 REDIRECT_URI = os.getenv("redirect_uri")
 SPOTIFY_SCOPE = "user-library-read"
-spotipy_client = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SPOTIFY_SCOPE, client_id=CLIENT_ID,
-                                                           client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI))
+spotipy_client = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SPOTIFY_SCOPE, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI))
 USER_ID = spotipy_client.current_user()["id"]
 
 def main():
     playlists = get_all_playlists()
     for playlist in playlists:
-        if playlist["name"] == "sldl":
-            # TODO: this only gets the first 100 tracks
+        if playlist["name"] == "test":
             get_all_playlist_tracks(playlist["id"])
-            # tracks = spotipy_client.playlist_tracks(playlist_id=playlist["id"])
-            # print(len(tracks["items"]))
-            # dump_json(tracks, "tracks.json")
+            create_vdjfolder_from_playlist(playlist_id=playlist["id"], vdjfolder_filepath="output/test.xml")
 
-def dump_json(contents, filename):
-    with open("assets/" + filename, "w", encoding="utf-8") as file:
-        json.dump(contents, file)
-
-def get_all_playlists(json_filename=None):
+def get_all_playlists(json_filepath=None):
     playlists_info = spotipy_client.user_playlists(USER_ID, limit=1)
     num_playlists = playlists_info["total"]
 
@@ -40,12 +35,12 @@ def get_all_playlists(json_filename=None):
         all_playlists.extend(new_playlists["items"])
         offset += 50
 
-    if json_filename is not None:
-        dump_json(all_playlists, json_filename)
+    if json_filepath is not None:
+        dump_json(all_playlists, json_filepath)
 
     return all_playlists
 
-def get_all_playlist_tracks(playlist_id, json_filename=None):
+def get_all_playlist_tracks(playlist_id, json_filepath=None):
     all_tracks = []
     offset = 0
 
@@ -57,8 +52,34 @@ def get_all_playlist_tracks(playlist_id, json_filename=None):
         if len(response["items"]) < 100:
             break
         
-    if json_filename is not None:
-        dump_json(all_tracks, json_filename)
+    if json_filepath is not None:
+        dump_json(all_tracks, json_filepath)
+
+def dump_json(contents, json_filepath):
+    with open(json_filepath, "w", encoding="utf-8") as file:
+        json.dump(contents, file)
+
+# creates a .vdjfolder file from a playlist_id
+def create_vdjfolder_from_playlist(playlist_id, vdjfolder_filepath):
+    # create the root and song entries
+    root = ET.Element("VirtualFolder", noDuplicates="no")
+    ET.SubElement(root, "song1", path="D:/path1.mp3", title="hello title1", artist="hello artist1")
+    ET.SubElement(root, "song2", path="D:/path2.mp3", title="hello title2", artist="hello artist2")
+
+    # create the output_dir if it doesnt already exist
+    output_dir = os.path.dirname(vdjfolder_filepath)
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    
+    # reparse the string with minidom to get formatting
+    rough_string = ET.tostring(root, encoding="utf-8")
+    reparsed = minidom.parseString(rough_string)
+    pretty_xml = reparsed.toprettyxml(indent="    ")
+
+    # write the header and formatted xml
+    with open(vdjfolder_filepath, "w", encoding="utf-8") as file:
+        file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        file.write(pretty_xml.split('\n', 1)[1])
+
 
 if __name__ == "__main__":
     main()
